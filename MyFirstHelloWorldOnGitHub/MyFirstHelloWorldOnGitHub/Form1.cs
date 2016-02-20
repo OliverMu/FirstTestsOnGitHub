ee444479a82541ad82a48aa28d9f5ec55334f157
OliverMu <oliver.mueller.1000@googlemail.com>
2016-02-20 23:44:41 +0100

    Added a function to Button Click.

--- MyFirstHelloWorldOnGitHub/MyFirstHelloWorldOnGitHub/Form1.cs
+++ MyFirstHelloWorldOnGitHub/MyFirstHelloWorldOnGitHub/Form1.cs
@@ -16,5 +16,10 @@ namespace MyFirstHelloWorldOnGitHub
 		{
 			InitializeComponent();
 		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Hello World!");
+		}
 	}
 }
